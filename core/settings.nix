ce8743875e5b76d810cc7a90fb530ca10a2b6e3d
Vostanis <kimonvostanis@gmail.com>
2025-04-30 13:15:02 +0100

    changing display & desktopmanagers

--- core/settings.nix
+++ core/settings.nix
@@ -54,7 +54,8 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
