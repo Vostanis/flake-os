861a9470518a60318c34607a6aedd8d5258b41e8
Vostanis <kimonvostanis@gmail.com>
2025-05-09 16:10:41 +0100

    python version 3.12 added with data science packages

--- core/programming_languages.nix
+++ core/programming_languages.nix
@@ -3,6 +3,24 @@
 {
   nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
   environment.systemPackages = with pkgs; [
+    # Python (version 3.12)
+    (python312.withPackages (ps: with ps; [
+      altair
+      jupyter
+      matplotlib
+      numpy
+      opencv4
+      polars
+      psycopg2 # dependency of sqlalchemy
+      requests
+      scipy
+      scikit-learn
+      sqlalchemy
+      torch
+      torchvision
+      xgboost
+    ]))
+
     # Rust
     rust-bin.stable.latest.default
     openssl
